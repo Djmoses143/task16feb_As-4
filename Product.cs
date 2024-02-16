ba4a4a883c7f10eeba263bb35059629af47e9411
john <john123@gmail.com>
2024-02-16 15:07:29 +0530

    hi

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task16feb
+{
+    public class Product
+    {
+        public string? PName { get; set; }
+        public int PPrice {  get; set; }
+        public string? PBrand { get; set; }
+        public DateTime MfgDate { get; set; }
+        public DateTime ExpDate { get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using task16feb;
+
+Console.WriteLine("<=== Welcome to Products Page ===>");
+
+     List<Product> products = new List<Product>()
+        {
+            new Product(){PName="Hair Gel",PPrice=67,PBrand="SetWet",MfgDate=new DateTime(day:15,month:01,year:2012),ExpDate=new DateTime(day:15,month:12,year:2012)},
+            new Product(){PName="Deodrant",PPrice=158,PBrand="Axe",MfgDate=new DateTime(day:28,month:01,year:2012),ExpDate=new DateTime(day:15,month:11,year:2012)},
+            new Product(){PName="Soapl",PPrice=31,PBrand="Santoor",MfgDate=new DateTime(day:06,month:06,year:2011),ExpDate=new DateTime(day:30,month:08,year:2012)},
+            new Product(){PName="Talk",PPrice=67,PBrand="Ponds",MfgDate=new DateTime(day:18,month:02,year:2012),ExpDate=new DateTime(day:31,month:12,year:2012)},
+        };
+  
+        foreach (Product product in products)
+        {
+           
+            //Console.WriteLine(product.PName+"\t"+ product.PPrice + "\t" + product.PBrand + "\t" + product.MfgDate + "\t" + product.ExpDate);
+            Console.WriteLine(product.PName);
+            Console.WriteLine(product.PPrice);
+            Console.WriteLine(product.PBrand);
+            Console.WriteLine(product.MfgDate);
+            Console.WriteLine(product.ExpDate);
+        }
+
+    
